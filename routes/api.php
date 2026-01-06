a8e775c02ef6268388c1f9b615310c2e61caab9f
Willian Andres Almendras Calizaya <willian.a.almendras@gmail.com>
2026-01-05 23:18:17 -0400

    ini

---
+++ routes/api.php
@@ -0,0 +1,9 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Ruta de prueba (Ping)
+Route::get('/ping', function () {
+    return response()->json(['message' => 'pong']);
+});
