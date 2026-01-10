e05630eb6a1fff4ba8515b4308c0b17fe4b04566
Diaz <owenstokd@gmail.com>
2026-01-10 19:33:24 -0400

    mejora de ruta

--- routes/api.php
+++ routes/api.php
@@ -10,19 +10,19 @@ Route::get('/ping', function () {
 });
 
 Route::prefix('v1')->group(function () {
-    Route::prefix('negocios')->group(function () {
+    Route::prefix('negocios/{negocioId}')->name('negocios.')->group(function () {
         // CRUD de negocios
-        Route::get('/', 'App\Http\Controllers\NegocioController@index');
+       /*  Route::get('/', 'App\Http\Controllers\NegocioController@index');
         Route::get('/{negocioId}', 'App\Http\Controllers\NegocioController@show');
         Route::post('/', 'App\Http\Controllers\NegocioController@store');
         Route::put('/{negocioId}', 'App\Http\Controllers\NegocioController@update');
-        Route::delete('/{negocioId}', 'App\Http\Controllers\NegocioController@destroy');
+        Route::delete('/{negocioId}', 'App\Http\Controllers\NegocioController@destroy'); */
 
         // Rutas de productos bajo un negocio específico
-        Route::prefix('{negocioId}/products')->group(function () {
-        Route::get('/', [productoController::class, 'index']); // obetener todos los productos de un negocio
-        Route::get('/categoria', [productoController::class, 'showCategoria']);   // obtener productos por categoría
-        Route::get('/{producto_id}', [productoController::class, 'show']);   // obtener un producto específico de un negocio
+        Route::prefix('/products')->group(function () {
+            Route::get('/', [productoController::class, 'index']); // obetener todos los productos de un negocio
+            Route::get('/categoria', [productoController::class, 'showCategoria']);   // obtener productos por categoría, mediante query params
+            Route::get('/{producto_id}', [productoController::class, 'show']);   // obtener un producto específico de un negocio
         
         }); 
     });
