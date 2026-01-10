021e50520f199115297a19adf5477f31ac4b7694
Diaz <owenstokd@gmail.com>
2026-01-10 11:02:38 -0400

    ft/obtener productos por categoria

--- app/Http/Controllers/Api/productoController.php
+++ app/Http/Controllers/Api/productoController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Categoria;
 use Illuminate\Http\Request;
 use App\Models\Producto;
 
@@ -40,5 +41,22 @@ class productoController extends Controller
         ];
         // Lógica para obtener y retornar un producto específico por su ID
         return response()->json($data, 200);
+    }
+
+    public function showCategoria(Request $request)
+    {
+        $categoria = $request->get('name'); // obtener el nombre de la categoría desde los parámetros de la solicitud
+        $id_Categoria = Categoria::where('nombre', $categoria)->value('id'); // obtener el ID de la categoría por su nombre
+        $productos = Producto::where('id_categoria', $id_Categoria)->get(); // obtener productos por categoría
+
+        if ($productos->isEmpty()) {
+            $data = [
+                'message' => 'No hay productos disponibles en esta categoría',
+                'status' => 200,
+            ];
+            return response()->json($data, 404);
+        } else {
+            return response()->json(['products' => $productos]);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,8 +18,12 @@ Route::prefix('v1')->group(function () {
         Route::put('/{id}', 'App\Http\Controllers\NegocioController@update');
         Route::delete('/{id}', 'App\Http\Controllers\NegocioController@destroy');
 
-        Route::get('/{id}/products', [productoController::class, 'index']); // obetener todos los productos de un negocio
-        Route::get('/{id}/products/{producto_id}', [productoController::class, 'show']);   // obtener un producto específico de un negocio
+        // Rutas de productos bajo un negocio específico
+        Route::prefix('{id}/products')->group(function () {
+        Route::get('/', [productoController::class, 'index']); // obetener todos los productos de un negocio
+        Route::get('/categoria', [productoController::class, 'showCategoria']);   // obtener productos por categoría
+        Route::get('/{producto_id}', [productoController::class, 'show']);   // obtener un producto específico de un negocio
+        
+        }); 
     });
-     
 });
