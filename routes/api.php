2da14c37de706ac64623946db282e6a55a579be6
Saraqch <quispechoquesara@gmail.com>
2026-01-11 21:29:31 -0400

    feat: Add confirmar-pedido route to API

--- routes/api.php
+++ routes/api.php
@@ -12,5 +12,6 @@ Route::get('/ping', function () {
 // Rutas del módulo MessagingOrders - Chat IA
 Route::prefix('ai/chat')->group(function () {
     Route::post('/send', [ChatController::class, 'send']);
+    Route::post('/confirmar-pedido', [ChatController::class, 'confirmarPedido']);
     Route::get('/status', [ChatController::class, 'status']);
 });
