ab572c18b64d89da9df055711b1af3d8e111e3ec
Diaz <owenstokd@gmail.com>
2026-01-28 18:32:44 -0400

    merge productosapi develop

--- routes/api.php
+++ routes/api.php
@@ -45,7 +45,7 @@ Route::prefix('v1')->group(function () {
         Route::post('/login', 'login');
     });
 
-    /* Route::middleware('auth:sanctum')->group(function () {
+    Route::middleware('auth:sanctum')->group(function () {
         Route::get('/user', fn (Request $request) => $request->user());
 
         Route::prefix('negocios')->controller(NegocioController::class)->group(function () {
@@ -55,7 +55,7 @@ Route::prefix('v1')->group(function () {
             // Futuro: Route::put('/{id}', 'update');
             // Futuro: Route::delete('/{id}', 'destroy');
         });
-    }); */
+    }); 
 });
 
 Route::prefix('v2')->group(function () {
@@ -80,19 +80,7 @@ Route::prefix('v1')->group(function () {
             Route::get('/', [productoController::class, 'index']); // listar todos los productos de un negocio
             Route::get('/categoria', [productoController::class, 'showCategoria']); // obtener productos por categoría
             Route::get('/{producto_id}', [productoController::class, 'show']); // obtener un producto específico
-        Route::get('/', function (Request $request, $negocioId) {
-            return response()->json(['message' => "Negocio ID: $negocioId"]);
         });
-        Route::prefix('/products')->group(function () {
-            Route::get('/', [productoController::class, 'index']);
-            Route::get('/categoria', [productoController::class, 'showCategoria']);
-            Route::get('/{producto_id}', [productoController::class, 'show']);
-
-            Route::post('/', [productoController::class, 'store']);
-            Route::put('/{producto_id}', [productoController::class, 'update']);
-            Route::delete('/{producto_id}', [productoController::class, 'destroy']);
-        });
-
         Route::get('/promociones', [promocionController::class, 'index']);
     });
 });
