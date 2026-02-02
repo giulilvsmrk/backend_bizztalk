4fa7e7bc9b2c0c2ede571176b06c84b7850fd9b0
Saraqch <quispechoquesara@gmail.com>
2026-02-02 09:59:38 -0400

    fix: formato de alerta de stock

--- app/Services/ChatbotBusinessResolver.php
+++ app/Services/ChatbotBusinessResolver.php
@@ -152,8 +152,8 @@ class ChatbotBusinessResolver
         
         foreach ($productos as $producto) {
             $stockInfo = '';
-            if ($producto['stock']['cantidad'] < 10) {
-                $stockInfo = "Solo {$producto['stock']['cantidad']} disponibles";
+            if ($producto['stock']['cantidad'] < 20) {
+                $stockInfo = " SOLO {$producto['stock']['cantidad']} DISPONIBLES";
             }
             $lineas[] = "• {$producto['nombre']} - {$producto['precio']}{$stockInfo}\n  {$producto['descripcion']}";
         }
